5111f5d8f5243055b5dff126ee40a12f30b196a6
Moondip Konwar <moondipkonwar9@gmail.com>
2025-03-22 09:53:18 +0530

    Header setup

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from tkinter.constants import *
 
 class App(tk.Tk):
     def __init__(self, _title:str, _size:tuple):
+        super().__init__()
 
         #Window
-        super().__init__()
-        
         self.title(_title)
         self.geometry(f'{_size[0]}x{_size[1]}')
-        
-        #Start
-        self.mainloop()
 
+        #Frames
+        self.header = Header(self)
+        self.header.pack(expand=True, fill='x', anchor='n')
+
+class Header(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+
+        #Add button #Add entry
+        self.add_button:ttk.Button = ttk.Button(self, text='Add Task')        
+        self.add_entry:ttk.Entry = ttk.Entry(self)
+
+        self.add_button.pack(expand=True, fill='x', side='left')
+        self.add_entry.pack(expand=True, fill='x', side='right', ipady=4)
 
 
 app = App('World\'s greatest Todo App', (500,500))
 
+app.mainloop()
